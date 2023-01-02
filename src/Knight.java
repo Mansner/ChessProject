83e75e5da9c95c299c37cda2db2e3d6769048245
junismank <elliot.mankowitz@gmail.com>
2023-01-02 13:56:00 +0100

    hh

--- src/Knight.java
+++ src/Knight.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Knight extends Piece{
@@ -9,9 +10,72 @@ public class Knight extends Piece{
         } else {
             this.value = 7;
         }
+
     }
+    public ArrayList<ArrayList<Integer>> getLegalMoves(Piece[][] board, int i, int j) {
+        System.out.println("update");
+        int x;
+        int y;
+        if(i<7){
+            if (j<6){
+                if (board[i + 1][j + 2]!=null)
+                {if (board[i + 1][j + 2].isWhite){
+                }
+                else{System.out.println("update");
+                    this.addMove(1,2);}
+                }
+                else{
+                System.out.println("update");
+                this.addMove(1,2);}
+
+            if (j>1){
+                if (board[i + 1][j - 2]!=null)
+                {if (board[i + 1][j - 2].isWhite){
+                }
+                else{System.out.println("update");
+                    this.addMove(1,-2);}
+                }
+                else{
+                    System.out.println("update");
+                    this.addMove(1,-2);}
+            }
+            if (i<6){
+                if (j<7){
+                    if (board[i + 2][j + 1]!=null)
+                    {if (board[i + 2][j + 1].isWhite){
+                    }
+                    else{System.out.println("update");
+                        this.addMove(2,1);}
+                    }
+                    else{
+                        System.out.println("update");
+                        this.addMove(2,1);}
+                    }
+
+                if (j>0){
+                    System.out.println("update4");
+                    this.addMove(2,-1);
+        }}}}
+
+            if (i>0){
+                if (j<7){
+                    System.out.println("update");
+                    this.addMove(-1,2);}
+                if (j>0) {
+                    System.out.println("update");
+                    this.addMove( -1, -2);
+            }
+            if (i>1){
+                if (j<7){
+                    System.out.println("update");
+                    this.addMove(-2,1);}
+                if (j>0) {
+                    System.out.println("update");
+                    this.addMove(-2, -1);
+                }}}
+        return legalMoves;
 
 
-}
+}}
 
 
